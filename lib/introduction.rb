889b9185c1f286e7f191709c498c2f59a8e0b097
Matt Mason <masonmj10@gmail.com>
2020-07-07 19:48:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,4 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
 
-introduction("Josh")
 
 def introduction_with_lagnuage(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
